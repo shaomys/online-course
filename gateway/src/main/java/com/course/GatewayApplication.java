5945c0d0ac5732eb04df5a40ed7470929db9a3a2
shaomys <2042372801@qq.com>
2020-10-09 16:32:13 +0800

    增加gateway模块

--- gateway/src/main/java/com/course/GatewayApplication.java
+++ gateway/src/main/java/com/course/GatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SystemApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SystemApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
